f9eb2191eb7bba206b608945b452dd66a58258b5
BertoSC <a23albertogc@iessanclemente.net>
2024-10-21 21:31:57 +0200

    trivial app

--- src/main/java/org/example/AppTrivial.java
+++ src/main/java/org/example/AppTrivial.java
@@ -25,7 +25,9 @@ public class AppTrivial {
                 .setTipoPregunta(TipoPregunta.MULTIPLE)
                 .setDificultad(Dificultad.EASY);
 
+       /* //EJERCICIO A
         Gson builder = new GsonBuilder()
+                .setPrettyPrinting()
                 .registerTypeAdapter(TipoPregunta.class, new JsonSerializer<TipoPregunta> (){
                     @Override
                     public JsonElement serialize(TipoPregunta tipoPregunta, Type type, JsonSerializationContext jsonSerializationContext) {
@@ -37,7 +39,71 @@ public class AppTrivial {
                 }).create();
 
         System.out.println(builder.toJson(preguntaMultiple));
+*/
 
+        /*EJERCICIO B
+        Gson builder = new GsonBuilder()
+                .registerTypeAdapter(TipoPregunta.class, new JsonSerializer<TipoPregunta> () {
+                    @Override
+                    public JsonElement serialize(TipoPregunta tipoPregunta, Type type, JsonSerializationContext jsonSerializationContext) {
+
+                        return new JsonPrimitive(tipoPregunta.toString().substring(0, 1).toUpperCase() + tipoPregunta.toString().substring(1).toLowerCase());
+                    }
+
+
+                }).create();
+
+        System.out.println(builder.toJson(preguntaMultiple));
+        */
+
+        /*//EJERCICIO C a
+        Gson builder = new GsonBuilder()
+                .registerTypeAdapter(Dificultad.class, new JsonSerializer<Dificultad>() {
+                    @Override
+                    public JsonElement serialize(Dificultad dificultad, Type type, JsonSerializationContext jsonSerializationContext) {
+                        return new JsonPrimitive(dificultad.getDificultad().toLowerCase());
+                    }
+                }).create();
+
+        System.out.println(builder.toJson(preguntaMultiple));
+        //EJERCICIO C b
+        Gson builder = new GsonBuilder()
+                .registerTypeAdapter(Dificultad.class, new JsonSerializer<Dificultad>() {
+                    @Override
+                    public JsonElement serialize(Dificultad dificultad, Type type, JsonSerializationContext jsonSerializationContext) {
+                        return new JsonPrimitive(dificultad.toString().substring(0,1).toUpperCase()+ dificultad.toString().substring(1).toLowerCase());
+                    }
+                }).create();
+        System.out.println(builder.toJson(preguntaMultiple));
+        */
+
+        /*  EJERCICIO D (No creo que este bien)
+        Gson builder = new GsonBuilder()
+                .registerTypeAdapter(Categoria.class, new JsonSerializer<Categoria>() {
+                    @Override
+                    public JsonElement serialize(Categoria categoria, Type type, JsonSerializationContext jsonSerializationContext) {
+                        return new JsonPrimitive(categoria.getNome());
+                    }
+                }).create();
+
+        System.out.println(builder.toJson(preguntaMultiple));
+
+
+         */
+
+        // EJERCICIO F
+
+        Gson builder = new GsonBuilder()
+                .registerTypeAdapter(Pregunta.class, new JsonSerializer<Pregunta>() {
+                    @Override
+                    public JsonElement serialize(Pregunta pregunta, Type type, JsonSerializationContext jsonSerializationContext) {
+                        JsonObject  js = new JsonObject();
+                        js.add("type", new JsonPrimitive(pregunta.getTipoPregunta().getTipoPregunta().toLowerCase()));
+                        js.add()
+
+                        return null;
+                    }
+                }).create();
 
 
     }
